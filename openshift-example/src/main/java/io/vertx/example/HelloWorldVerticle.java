cee692e773d1d8df972ff3a01c4f9033c9ac3075
Fifcan <fifcan@fifcan.org>
2019-04-11 22:15:11 +0200

    Update HelloWorldVerticle.java

--- openshift-example/src/main/java/io/vertx/example/HelloWorldVerticle.java
+++ openshift-example/src/main/java/io/vertx/example/HelloWorldVerticle.java
@@ -8,8 +8,7 @@ public class HelloWorldVerticle extends AbstractVerticle {
   @Override
   public void start() throws Exception {
     vertx.createHttpServer().requestHandler(req -> req.response().end("Hello World!"))
-    .listen(
-        Integer.getInteger("http.port"), System.getProperty("http.address"));
+    .listen(System.getProperty("http.port"));
   }
 }
 
